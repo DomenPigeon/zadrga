68fcd179738e6cf25d7368b1629cbcda90ad1c8c
DomenGolob <domen.golob@outfit7.com>
2022-08-29 22:36:55 +0200

    Working local ftp server

--- CloudServer/Program.cs
+++ CloudServer/Program.cs
@@ -1,8 +1,16 @@
+using FubarDev.FtpServer;
+using FubarDev.FtpServer.FileSystem.DotNet;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.Configure<DotNetFileSystemOptions>(opt => opt.RootPath = Path.Combine("wwwroot", "drive"));
+builder.Services.AddFtpServer(builder => builder
+    .UseDotNetFileSystem()
+    .EnableAnonymousAuthentication());
+builder.Services.Configure<FtpServerOptions>(opt => opt.ServerAddress = "localhost");
 
 var app = builder.Build();
 
@@ -21,5 +29,8 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+var ftpHost = app.Services.GetRequiredService<IFtpServerHost>();
+await ftpHost.StartAsync();
 
 app.Run();
